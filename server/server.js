7e8781c98c4d0b26bb01893e5293971d32dca45a
TrojanVoid <kiroglu.berk@gmail.com>
2024-01-06 03:53:47 +0300

    separated API functions from server, then deployed on it

--- server/server.js
+++ server/server.js
@@ -2,20 +2,13 @@ const express = require('express');
 const Sequelize = require('sequelize');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 const session = require('express-session');
-const crypto = require('crypto');
 const MySQLStore = require('express-mysql-session')(session);
 const path = require('path');
-
 const sessionConfig = require('./config/sessionConfig.json');
 const envConfig = require('./config/envConfig.json');
 
 
-const getDynamicSecretKeyForUser = (user) => {
-    const secretKeyLength = 6; // You can adjust the length as needed
-    return crypto.randomInt(0, Math.pow(10, secretKeyLength)).toString();
-};
 
 const sessionStoreMySQLOptions = {
     database: sessionConfig.sessionStore.database,
@@ -58,6 +51,7 @@ const sessionOptions = {
 const db = require("./models");
 const {user} = require('./models');
 const { Server } = require('http');
+const api = require('./api/api');
 const app = express();
 
 
@@ -69,100 +63,17 @@ app.use(cors({
     credentials: true,
     allowedHeaders: "X-PINGOTHER, Content-Type",   
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-})); // Enable CORS
+}));
 app.use(bodyParser.json());
 app.use(session(sessionOptions));
 
+Object.keys(api.post).forEach(apiFunctionName => app.post(apiFunctionName, api.post[apiFunctionName]));
+Object.keys(api.get).forEach(apiFunctionName => app.get(apiFunctionName, api.get[apiFunctionName]));
 
 // Maintain a list of revoked tokens
 const revokedTokens = new Set(); 
 
-// Handle login form submission
-app.post('/api/authenticate-client', async (req, res) => {
 
-    console.log("Authentication request received...");
-
-    if(req.session.loggedUser)
-    {
-        res.status(200).json({status: 'success', message: 'Already logged in'});
-    }
-    
-    const { email, password } = req.body;
-    try {
-        const userModel = await user.findOne({ where: { email, password } });
-        if (userModel) {
-            const dynamicSecretKey = getDynamicSecretKeyForUser(userModel);
-            console.log(`Dynamic secret key for ${userModel.email}: ${dynamicSecretKey}`);
-            const payload = { userId: userModel.userId, email: userModel.email };
-            const token = jwt.sign(payload, dynamicSecretKey, { expiresIn: '5m' });
-            req.session.loggedUser = {
-                email : userModel.email,
-                userId : userModel.userId,
-                role : userModel.role,
-                tenantId : userModel.tenantId
-            }
-            await req.session.save();
-            console.log(req.session); 
-            res.json({
-                "userId": req.session.loggedUser.userId,
-                "status" : "success"
-            })
-            .send();
-            
-        } else {
-            console.log("Login failed due to invalid credentials.");
-            res.json({ 
-                status: "invalidCredentials",
-                message: 'Login failed due to invalid credentials.'})
-                .send();
-        }
-    } catch (error) {
-        console.error("Error logging in: ", error);
-        res.json({ status: 500, message: 'Error logging in' }.send());
-    }
-});
-
-app.post('/api/checkLoggedInStatus', async(req, res)=> {
-    console.log("client request cookies: ", req.cookies);
-    console.log(req.sessionID);
-    console.log(req.session);
-    res.status(200).json({status: 'success', isLoggedIn: req.session.loggedUser ? true : false});
-}); 
-
-app.post('/api/registerUser', async (req,res) => {
-    const {email, password, firstName, middleName, lastName} = req.body;
-    try{
-        const userModel = await user.findOne({where: {email}});
-        if(! userModel){
-            const newUser = await user.create({
-                email,
-                password, 
-                firstName,
-                middleName,
-                lastName
-            });
-
-            res.json({
-                status: "success",
-                message: "User registered successfully",
-                userId: newUser.userId,
-            }).send();
-        }
-        else{
-            res.json({
-                status: "userExists",
-                message: "User already exists"
-            }).send();
-        }
-    }
-    catch(error)
-    {
-        console.error("Error checking user existence in database upon registration: ", error);
-        res.json({status: 500, message: "Registration Error"}).send();
-    }
-
-    
-})
 
 
 db.sequelize.sync().then((req) => {
